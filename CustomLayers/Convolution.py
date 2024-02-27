fbf5e3efcf1837c30bbb4047a22dfa6faafead88
Biplav Poudel <biplavpoudel764@gmail.com>
2024-02-27 09:40:53 +0545

    model created but fingers crossed it is a mess

--- CustomLayers/Convolution.py
+++ CustomLayers/Convolution.py
@@ -5,13 +5,14 @@ import Loss
 
 
 class Convolution(nn.Module):
-    def __init__(self, filters, kernel_size, input_shape):
+    def __init__(self, filters, kernel_size, input_shape, stride=1):
         super(Convolution, self).__init__()
         input_depth, input_height, input_width = input_shape
         self.input_shape = input_shape
         self.input_depth = input_depth
         self.filters = filters
         self.kernel_size = kernel_size
+        self.stride = stride
         self.output_shape = (filters, input_height - kernel_size + 1, input_width - kernel_size + 1)
         # for valid convolution, output size is: (image_dimension - kernel_dimension +1)
         # for full convolution, output size is: (image_dimension + kernel_dimension -1)
@@ -22,14 +23,14 @@ class Convolution(nn.Module):
 
     def forward(self, input):
         input = input
-        output = F.conv2d(input, self.kernels, bias=self.biases, stride=1, padding=0)
+        output = F.conv2d(input, self.kernels, bias=self.biases, stride=self.stride, padding=0)
         return output
-
-    def backward(self, output_gradient, learning_rate):
-        loss_function = Loss.CrossEntropyLoss()
-        loss = loss_function(self.forward(self.input), output_gradient)
-        optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        return loss.item()
+    #
+    # def backward(self, output_gradient, learning_rate):
+    #     loss_function = Loss.CrossEntropyLoss()
+    #     loss = loss_function(self.forward(self.input), output_gradient)
+    #     optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+    #     optimizer.zero_grad()
+    #     loss.backward()
+    #     optimizer.step()
+    #     return loss.item()
--- model.py
+++ model.py
@@ -1,7 +1,31 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from CustomLayers import Convolution, Activation, Loss
+from CustomLayers.Convolution import Convolution
+from CustomLayers.Activation import RectifiedLinearUnit
+from CustomLayers.MaxPool import MaxPool2d
+from CustomLayers.Loss import CrossEntropyLoss
+from CustomLayers.Dropout import Dropout
+from CustomLayers.Flatten import Flatten
+from CustomLayers.Dense import Dense
+from CustomLayers.Activation import LogSoftmax
 
 
-
+class ASLClassifier(nn.Module):
+    def __init__(self, num_classes=26):
+        super(ASLClassifier, self).__init__()
+        self.conv1 = Convolution(filters=32, kernel_size=3, input_shape=(3, 224, 224))
+        self.activation1 = RectifiedLinearUnit()
+        self.pool1 = MaxPool2d(pool_size=2, stride=2)
+        self.conv2 = Convolution(filters=64, kernel_size=3, input_shape=(32, 111, 111))
+        self.activation2 = RectifiedLinearUnit()
+        self.pool2 = MaxPool2d(pool_size=2, stride=2)
+        self.conv3 = Convolution(filters=64, kernel_size=3, input_shape=(32, 54, 54))
+        self.activation3 = RectifiedLinearUnit()
+        self.pool3 = MaxPool2d(pool_size=2, stride=2)    # Output Shape: (64, 26, 26)
+        self.flatten = Flatten()
+        self.dense1 = Dense(64*26*26, 128)
+        self.activation4 = RectifiedLinearUnit()
+        self.dropout = Dropout(p=0.5)
+        self.dense2 = Dense(128, 26)
+        self.activation5 = LogSoftmax()
