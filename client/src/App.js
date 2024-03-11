b59445d3b2eda256e0a0c92e76b13ff69d43a300
Salina <salina.gurung009@gamil.com>
2024-03-11 23:57:29 +0545

    REact component added

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ASLInterface from './ASLInterface';
+
+function App() {
+  return (
+    <div className="App">
+      <ASLInterface />
+    </div>
+  );
+}
+
+export default App;
