ff5c790d1ce0d7a194b0090c601114516eb93fbe
Salina <salina.gurung009@gamil.com>
2024-03-12 00:50:49 +0545

    UI interface added

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import torch
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():  # put application's code here
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     return str(device)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
